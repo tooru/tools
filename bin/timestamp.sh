47c704437ffd1ac9e2ecd7bdcb64edb11bb4825d
Tooru Takahashi <tooru.takahashi134@gmail.com>
2021-02-28 15:49:51 +0900

    support #1

--- bin/timestamp.sh
+++ bin/timestamp.sh
@@ -20,6 +20,7 @@ mkdir -p timestamp || abort
 DATE=$(identify -format '%[exif:dateTimeOriginal]' "$1" | ruby -rtime -e 'puts gets.sub(/(\d+):(\d+):(\d+) \d+:\d+:\d+/, "\\1/\\2/\\3")')
 
 convert "$1" \
+-auto-orient \
 -stroke '#000c' -strokewidth 10 -pointsize 125 -gravity southeast -annotate +200+200 $DATE \
 -stroke none -fill orange -pointsize 125 -gravity southeast -annotate +200+200 $DATE \
 timestamp/"$1"
